92734a92fa0e25b479b6ad742864a30359e16475
Leonardo Ferrero-Merlino <leonardo.ferrerom@edu.unito.it>
2023-09-26 10:00:45 +0200

    Work with Ciao :D

--- Algorithm/Huffman.cs
+++ Algorithm/Huffman.cs
@@ -26,95 +26,6 @@ public class Huffman {
         GenerateUncompressedFile(filePath, tree);
     }
 
-    private List<Node> ExtractListFromFile(string filePath) {
-        List<Node> result = new List<Node>();
-        int nOfChars = 0;
-        int[] compareValues;
-        char[] values;
-            
-        if (!File.Exists(filePath)) throw new FileNotFoundException($"The file at the path {filePath} was not founded");
-        var treeString = new StringBuilder();
-        using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None)) {
-            // skip dirty byte
-            fs.Position = 1;
-            nOfChars = fs.ReadByte();
-            
-            compareValues = new int[nOfChars];
-            values = new char[nOfChars];
-            using (BinaryReader br = new BinaryReader(fs))
-            {
-                fs.Seek(2, SeekOrigin.Begin);
-                for (int i = 0; i < nOfChars; i++)
-                    compareValues[i] = br.ReadInt32();
-                
-                values = br.ReadChars(nOfChars);
-            }
-        }
-
-        for (int i = 0; i < nOfChars; i++)
-            result.Add(new Node(compareValues[i], values[i]));
-        
-        
-        return result.OrderBy(x => x.value).OrderBy(x => x.compareValue).ToList();
-    }
-    
-    private void GenerateUncompressedFile(string filePath, HTree tree) {
-        string newFilePath = filePath.Substring(0, filePath.LastIndexOf('.')) + "_dec.txt";
-        using (FileStream fr = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None)) {
-            // Read the dirty byte
-            bool hasDirtyByte = fr.ReadByte() == 1;
-            int fileContent = 5 + tree.GetNumberOfLeaves() * 5; //1 dirt byte + 4 n of items (int) + n of items * 4 (int) + n of item
-            fr.Seek(fileContent - 1, SeekOrigin.Begin);
-            
-            // Create the decompressed file
-            int bufferIndex = 0;
-            int bitsIndex = 0;
-            int fileIndex = 0;
-            Node lastVisitedNode = null;
-            using (FileStream fs = File.Create(newFilePath)) {
-                int byteRead;
-                while ((byteRead = fr.ReadByte()) != -1) {
-                    if (bufferIndex < BUFFER_LENGTH_WRITE) {
-                        wBuffer[bufferIndex] = (byte)byteRead;
-                        bufferIndex++;
-                    } else {
-                        BitArray bitsArray = new BitArray(wBuffer);
-                        while (bitsIndex < bitsArray.Length) {
-                            char? nextChar = tree.GetCharFromBits(bitsArray, ref bitsIndex, ref lastVisitedNode);
-                            if (nextChar != null) {
-                                wBufferOnFile[fileIndex] = (byte)nextChar.Value;
-                                fileIndex++;
-                            }
-                            if (fileIndex >= wBufferOnFile.Length) {
-                                fs.Write(wBufferOnFile, 0, readBufferFileToCompress.Length);
-                                fileIndex = 0;
-                            }
-                        }
-                    }
-                }
-                
-                if (bufferIndex != 0) {
-                    byte[] lastWBuffer = new byte[bufferIndex];
-                    Array.Copy(wBuffer, lastWBuffer, bufferIndex);
-                    BitArray bitsArray_2 = new BitArray(lastWBuffer);
-                    bitsIndex = 0;
-                    while (bitsIndex < bitsArray_2.Length) {
-                        char? nextChar = tree.GetCharFromBits(bitsArray_2, ref bitsIndex, ref lastVisitedNode);
-                        if (nextChar != null) {
-                            wBufferOnFile[fileIndex] = (byte)nextChar.Value;
-                            fileIndex++;
-                        }
-                        if (fileIndex >= wBufferOnFile.Length) {
-                            fs.Write(wBufferOnFile, 0, readBufferFileToCompress.Length);
-                            fileIndex = 0;
-                        }
-                    }
-                    fs.Write(wBufferOnFile, 0, fileIndex);
-                }
-            }
-        }
-    }
-
     private List<Node> GenerateList(string filePath) {
         List<Node> result = new List<Node>();
         Dictionary<char, int> cd = new Dictionary<char, int>();
@@ -186,9 +97,7 @@ public class Huffman {
         string newFilePath = filePath.Substring(0, filePath.LastIndexOf('.')) + ".mlh";
         using (FileStream destinationStream = File.Create(newFilePath)) {
             // Set the dirty byte
-            bool changeDirtyByte = false;
-            byte[] dirtyByte = new UTF8Encoding(true).GetBytes("1");
-            destinationStream.Write(dirtyByte, 0, dirtyByte.Length);
+            destinationStream.Write(new byte[1], 0, 1);
             
             // Set the number of occurrences
             destinationStream.Write(new byte[]{ tree.GetNumberOfLeaves() }, 0, sizeof(byte));
@@ -217,7 +126,7 @@ public class Huffman {
                                 readBufferIndex = 0;
                             }
 
-                            readBufferFileToCompress[readBufferIndex] = (byte)GetIntValueFromByteArray(bitBuffer);
+                            readBufferFileToCompress[readBufferIndex] = (byte)GetIntValueFromBitArray(bitBuffer);
                             readBufferIndex++;
 
                             bitIndex = 0;
@@ -226,33 +135,136 @@ public class Huffman {
                 }
             }
             
+            int nOfDirtyBytes = BIT_BUFFER_LENGTH - bitIndex - 1; 
             if (bitIndex != 0) {
                 bool substituteValue = !bitBuffer[bitIndex];
                 for (int i = bitIndex + 1; i < BIT_BUFFER_LENGTH; i++) {
                     bitBuffer[i] = substituteValue;
                 }
-                readBufferFileToCompress[readBufferIndex] = (byte)GetIntValueFromByteArray(bitBuffer);
+                readBufferFileToCompress[readBufferIndex] = (byte)GetIntValueFromBitArray(bitBuffer);
                 readBufferIndex++;
-            } else {
-                changeDirtyByte = true;
             }
                 
             // Buffer dump
             destinationStream.Write(readBufferFileToCompress, 0, readBufferIndex);
 
-            if (changeDirtyByte) {
+            if (nOfDirtyBytes != 0) {
                 destinationStream.Seek(0, SeekOrigin.Begin);
-                dirtyByte = new UTF8Encoding(true).GetBytes("0");
-                destinationStream.Write(dirtyByte, 0, dirtyByte.Length);
+                destinationStream.Write(new byte[]{ (byte)nOfDirtyBytes }, 0, 1);
             }
         }
     }
 
-    private int GetIntValueFromByteArray(BitArray r) {
+    private int GetIntValueFromBitArray(BitArray r) {
         int result = 0;
         for (int i = 0; i < r.Length; i++) {
             if (r[i]) result += Convert.ToInt32(Math.Pow(2, i));
         }
         return result;
+    }
+    
+    private List<Node> ExtractListFromFile(string filePath) {
+        List<Node> result = new List<Node>();
+        int nOfChars = 0;
+        int[] compareValues;
+        char[] values;
+            
+        if (!File.Exists(filePath)) throw new FileNotFoundException($"The file at the path {filePath} was not founded");
+        using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None)) {
+            // skip dirty byte
+            fs.Position = 1;
+            nOfChars = fs.ReadByte();
+            
+            compareValues = new int[nOfChars];
+            values = new char[nOfChars];
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                fs.Seek(2, SeekOrigin.Begin);
+                for (int i = 0; i < nOfChars; i++)
+                    compareValues[i] = br.ReadInt32();
+                
+                values = br.ReadChars(nOfChars);
+            }
+        }
+
+        for (int i = 0; i < nOfChars; i++)
+            result.Add(new Node(compareValues[i], values[i]));
+        
+        return result.OrderBy(x => x.value).OrderBy(x => x.compareValue).ToList();
+    }
+    
+    private void GenerateUncompressedFile(string filePath, HTree tree) {
+        string newFilePath = filePath.Substring(0, filePath.LastIndexOf('.')) + "_dec.txt";
+        using (FileStream fr = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None)) {
+            // Read the dirty byte
+            int nOfDirtyBits = fr.ReadByte();
+            int fileContent = 5 + tree.GetNumberOfLeaves() * 5; //1 n dirt byte + 4 n of items (int) + n of items * 4 (int) + n of item
+            fr.Seek(fileContent - 1, SeekOrigin.Begin);
+            
+            // Create the decompressed file
+            int bufferIndex = 0;
+            int bitsIndex = 0;
+            int fileIndex = 0;
+            Node lastVisitedNode = null;
+            BitArray bitsArray;
+            using (FileStream fs = File.Create(newFilePath)) {
+                int byteRead;
+                while ((byteRead = fr.ReadByte()) != -1) {
+                    if (bufferIndex < BUFFER_LENGTH_WRITE) {
+                        wBuffer[bufferIndex] = (byte)byteRead;
+                        bufferIndex++;
+                    } else {
+                        if (fs.Position == fs.Length) {
+                            BitArray tmpBitArray = new BitArray(wBuffer);
+                            bitsArray = new BitArray(wBuffer.Length * 8 - nOfDirtyBits);
+                            for (int i = 0; i < bitsArray.Length; i++)
+                                bitsArray[i] = tmpBitArray[i];
+                        } else {
+                            bitsArray = new BitArray(wBuffer);
+                        }
+                        
+                        while (bitsIndex < bitsArray.Length) {
+                            char? nextChar = tree.GetCharFromBits(bitsArray, ref bitsIndex, ref lastVisitedNode);
+                            if (nextChar != null) {
+                                wBufferOnFile[fileIndex] = (byte)nextChar.Value;
+                                fileIndex++;
+                            }
+                            if (fileIndex >= wBufferOnFile.Length) {
+                                fs.Write(wBufferOnFile, 0, readBufferFileToCompress.Length);
+                                fileIndex = 0;
+                            }
+                        }
+                    }
+                }
+                
+                if (bufferIndex != 0) {
+                    byte[] lastWBuffer = new byte[bufferIndex];
+                    Array.Copy(wBuffer, lastWBuffer, bufferIndex);
+
+                    if (fs.Position == fs.Length) {
+                        BitArray tmpBitArray = new BitArray(lastWBuffer);
+                        bitsArray = new BitArray(lastWBuffer.Length * 8 - nOfDirtyBits);
+                        for (int i = 0; i < bitsArray.Length; i++)
+                            bitsArray[i] = tmpBitArray[i];
+                    } else {
+                        bitsArray = new BitArray(lastWBuffer);
+                    }
+                    
+                    bitsIndex = 0;
+                    while (bitsIndex < bitsArray.Length) {
+                        char? nextChar = tree.GetCharFromBits(bitsArray, ref bitsIndex, ref lastVisitedNode);
+                        if (nextChar != null) {
+                            wBufferOnFile[fileIndex] = (byte)nextChar.Value;
+                            fileIndex++;
+                        }
+                        if (fileIndex >= wBufferOnFile.Length) {
+                            fs.Write(wBufferOnFile, 0, readBufferFileToCompress.Length);
+                            fileIndex = 0;
+                        }
+                    }
+                    fs.Write(wBufferOnFile, 0, fileIndex);
+                }
+            }
+        }
     }
 }
--- CompressionTest/Program.cs
+++ CompressionTest/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args) {
         Huffman cmp = new Huffman();
-        cmp.Compress(@"C:\Users\Lo\Desktop\Cmp\Test2.txt");
+        cmp.Compress(@"C:\Users\Lo\Desktop\Cmp\Test3.txt");
         Console.WriteLine("Finished compressing");
 
         Huffman ucmp = new Huffman();
-        ucmp.Uncompress(@"C:\Users\Lo\Desktop\Cmp\Test2.mlh");
+        ucmp.Uncompress(@"C:\Users\Lo\Desktop\Cmp\Test3.mlh");
         Console.WriteLine("Finished uncompressing");
     }
 }
